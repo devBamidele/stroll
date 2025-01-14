37a20a0a07b198716e1cd653ef2d2293911e94bc
Bamidele Ajewole <bamideledavid.femi@gmail.com>
2025-01-14 06:40:08 +0100

    FEAT [core] : Implement home page UI and add remaining tabs

--- lib/feature/home/home.dart
+++ lib/feature/home/home.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../constants/assets.dart';
+import 'chat/chat_screen.dart';
+import 'flame/flame_screen.dart';
+import 'match/match_screen.dart';
+import 'profile/profile_screen.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,8 +15,49 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final List<Widget> screens = const [
+    MatchScreen(),
+    FlameScreen(),
+    ChatScreen(),
+    ProfileScreen(),
+  ];
+
+  final Map<String, String> activeIcons = {
+    AppAssets.iconChat: AppAssets.iconChatActive,
+    AppAssets.iconFire: AppAssets.iconFireActive,
+  };
+
+  int selectedIndex = 1;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: screens[selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: selectedIndex,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        onTap: (index) => setState(() => selectedIndex = index),
+        items: [
+          _buildNavItem(AppAssets.iconCard, 0),
+          _buildNavItem(AppAssets.iconFire, 1),
+          _buildNavItem(AppAssets.iconChat, 2),
+          _buildNavItem(AppAssets.iconPerson, 3),
+        ],
+      ),
+    );
+  }
+
+  BottomNavigationBarItem _buildNavItem(String assetPath, int index) {
+    final iconToUse =
+        selectedIndex == index && activeIcons.containsKey(assetPath)
+            ? activeIcons[assetPath]!
+            : assetPath;
+
+    return BottomNavigationBarItem(
+      icon: SvgPicture.asset(iconToUse, height: 28),
+      label: "",
+    );
   }
 }
